c623ef5b37bf91d9244c923275ceaea2d49bc796
JulianoNode <julianonode@gmail.com>
2024-02-27 19:41:14 -0300

    AppPorcentagen, AppPlataforma e AppPorClick * Crud java *

---
+++ appPctgDesconto/src/main/java/com/jmr/app/repository/PctgDescontoRepository.java
@@ -0,0 +1,16 @@
+package com.jmr.app.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.jmr.app.domain.PctgDesconto;
+
+public interface PctgDescontoRepository extends JpaRepository<PctgDesconto, Long>{
+
+	@Query("select e from PctgDesconto e where e.desconto like :search%")
+	Page<PctgDesconto> findAllByDesconto(String search, Pageable pageable);
+
+	
+}
